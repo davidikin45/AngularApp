c3c2aacd3f7c1048d1bc04960785a76a698eeeda
David Ikin <davidikin45@gmail.com>
2018-11-24 17:17:34 +0400

    Hanfgire Launcher

--- src/DatingApp/AspNetCore.ApiBase/Hangfire/HangfireHelper.cs
+++ src/DatingApp/AspNetCore.ApiBase/Hangfire/HangfireHelper.cs
@@ -8,6 +8,7 @@ using Hangfire.SQLite;
 using Hangfire.SqlServer;
 using Hangfire.States;
 using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
 
 namespace AspNetCore.ApiBase.Hangfire
 {
@@ -79,6 +80,50 @@ namespace AspNetCore.ApiBase.Hangfire
 
             applicationLifetime.ApplicationStopping.Register(() => server.SendStop());
             applicationLifetime.ApplicationStopped.Register(() => server.Dispose());
+
+            var recurringJobManager = new RecurringJobManager(storage, backgroundJobFactory);
+
+            var backgroundJobClient = new BackgroundJobClient(storage, backgroundJobFactory, backgroundJobStateChanger);
+
+            return (server, recurringJobManager, backgroundJobClient);
+        }
+
+        public static (BackgroundJobServer server, IRecurringJobManager recurringJobManager, IBackgroundJobClient backgroundJobClient) StartHangfireServer(string connectionString, string serverName = null)
+        {
+            JobStorage storage;
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                storage = new MemoryStorage();
+            }
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
+            {
+                storage = new SQLiteStorage(connectionString);
+            }
+            else
+            {
+                storage = new SqlServerStorage(connectionString);
+            }
+
+            var options = new BackgroundJobServerOptions
+            {
+                ServerName = serverName,
+                Queues = new string[] { "default" }
+            };
+
+            var filterProvider = JobFilterProviders.Providers;
+            var activator = JobActivator.Current;
+
+            var backgroundJobFactory = new BackgroundJobFactory(filterProvider);
+            var performer = new BackgroundJobPerformer(filterProvider, activator);
+            var backgroundJobStateChanger = new BackgroundJobStateChanger(filterProvider);
+            IEnumerable<IBackgroundProcess> additionalProcesses = null;
+
+            var server = new BackgroundJobServer(options, storage, additionalProcesses,
+                filterProvider,
+                activator,
+                backgroundJobFactory,
+                performer,
+                backgroundJobStateChanger);
 
             var recurringJobManager = new RecurringJobManager(storage, backgroundJobFactory);
 
